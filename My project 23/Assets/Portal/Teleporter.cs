e82452394c40e7e5b1c4b59d7ffab6077e635135
leonidi86 <leonidi368@gmail.com>
2023-11-10 11:03:31 +0300

    код для телепорта

---
+++ My project 23/Assets/Portal/Teleporter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+    public Teleporter Other;
+
+    private void Start()
+    {
+        
+    }
+    
+    private void Update()
+    {
+        
+    }
+
+    private void Teleport(Transform obj)
+    {
+        // Position
+        Vector3 localPos = Other.transform.worldToLocalMatrix.MultiplyPoint3x4(obj.position);
+        localPos = new Vector3(-localPos.x, localPos.y, -localPos.z);
+        obj.position = transform.localToWorldMatrix.MultiplyPoint3x4(localPos);
+
+        // Rotation
+        Quaternion relativeRotation = Quaternion.Inverse(Other.transform.rotation) * obj.rotation;
+        obj.rotation = transform.rotation * relativeRotation;
+    }
+}
