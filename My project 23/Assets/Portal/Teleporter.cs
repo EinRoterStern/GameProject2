3e7eee4f41275784881e3c6fbe4e0c9d4ee1bbd5
leonidi86 <leonidi368@gmail.com>
2023-11-10 14:54:05 +0300

    редактирование телепорта

--- My project 23/Assets/Portal/Teleporter.cs
+++ My project 23/Assets/Portal/Teleporter.cs
@@ -1,29 +1,41 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Teleporter : MonoBehaviour
 {
     public Teleporter Other;
 
-    private void Start()
+    private void OnTriggerStay(Collider other)
     {
-        
-    }
-    
-    private void Update()
-    {
-        
+        float zPos = transform.InverseTransformPoint(other.transform.position).z;
+
+        if (zPos < 0)
+        {
+            Teleport(other.transform);
+        }
     }
 
     private void Teleport(Transform obj)
     {
-        // Position
-        Vector3 localPos = Other.transform.worldToLocalMatrix.MultiplyPoint3x4(obj.position);
-        localPos = new Vector3(-localPos.x, localPos.y, -localPos.z);
-        obj.position = transform.localToWorldMatrix.MultiplyPoint3x4(localPos);
+        if (Other == null)
+        {
+            Debug.LogError("Other Teleporter is not assigned.");
+            return;
+        }
 
-        // Rotation
-        Quaternion relativeRotation = Quaternion.Inverse(Other.transform.rotation) * obj.rotation;
-        obj.rotation = transform.rotation * relativeRotation;
+        // Use the position of the Other Teleporter
+        Vector3 localPos = Other.transform.worldToLocalMatrix.MultiplyPoint3x4(obj.position);
+        localPos = new Vector3(-(localPos.x), localPos.y, -(localPos.z));
+
+        // Adjust the y-coordinate based on the height of your world
+        float worldHeight = 20.0f; // Update with the actual height of your world
+        localPos.y = Mathf.Clamp(localPos.y, 0, worldHeight);
+
+        Vector3 newPosition = Other.transform.localToWorldMatrix.MultiplyPoint3x4(localPos) + Other.transform.forward * 5.0f;
+
+        // Set the new position
+        obj.position = newPosition;
+
+        // Set the new rotation
+        obj.rotation = Other.transform.rotation * Quaternion.Euler(0, 180, 0);
     }
 }
